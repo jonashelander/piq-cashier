5995324ef138c3d65b1c64a0b14a2d705719f99e
HELANDER <jonas.helander@worldline.com>
2023-02-12 11:44:37 +0100

    Added initial navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './styles/App.css';
 import _PaymentIQCashier from 'paymentiq-cashier-bootstrapper'
 import Cashier from './components/Cashier';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <div className='container'>
-        <Cashier />   
+      <Navbar />
+      <Cashier />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import '../styles/Navbar.css'
+
+
+function Navbar() {
+
+    return (
+        <header className="navbar">
+        </header>
+    );
+}
+
+export default Navbar;
