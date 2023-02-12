d14870f948c24551857aaf77f43afa16906045d9
Jonas <helanderjonas@gmail.com>
2023-02-12 20:42:58 +0100

    Navbar fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './styles/App.css';
-import Cashier from './components/Cashier';
-import Navbar from './components/Navbar';
+import "./styles/App.css";
+import Cashier from "./components/Cashier";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <div className='container'>
+    <div className="container">
       <Navbar />
       <Cashier />
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,16 @@
-import '../styles/Navbar.css'
-
+import "../styles/Navbar.css";
 
 function Navbar() {
+  return (
+    <header className="navbar">
+      <h1>PaymentIQ Casino Demo</h1>
+      <h3>Balance: $ 1000</h3>
 
-    return (
-        <header className="navbar">
-            <h1>PaymentIQ Casino Demo</h1>
-            <h3>Balance: $ 1000</h3>
-
-            <nav>
-                <a href='www.dn.se'>Login</a>
-            </nav>
-        </header>
-    );
+      <nav>
+        <a href="www.dn.se">Sing out</a>
+      </nav>
+    </header>
+  );
 }
 
 export default Navbar;
