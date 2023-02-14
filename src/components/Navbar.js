da03f49543a8c8e45d27cb8119d7c7ebf324cebd
Jonas <helanderjonas@gmail.com>
2023-02-14 11:26:04 +0100

    Added dynamic balance, taken fron useState

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import "../styles/Navbar.css";
 
 function Navbar() {
+  const [balance, setBalance] = useState(1000);
+
   return (
     <header className="navbar">
       <h1>PaymentIQ Casino Demo</h1>
-      <h3>Balance: $ 1000</h3>
+      <h3>Balance: $ {balance}</h3>
 
       <nav>
         <a href="www.dn.se">Sing out</a>
