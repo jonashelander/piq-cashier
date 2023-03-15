49a2e7cd1ef8ee6bbbb2b081ec348c82bd2a1f5d
HELANDER <jonas.helander@worldline.com>
2023-03-15 16:01:05 +0100

    Structure of signIn page done

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
 function SignUp() {
-    const [firstName, setFirstName] = useState("Jonas");
-    const [lastName, setlasttName] = useState("Helander");
-    const [country, setCountry] = useState("Sweden");
-    const [street, setStreet] = useState("Prajtejder");
-    const [zip, setZip] = useState("184 61");
+    const [firstName, setFirstName] = useState();
+    const [lastName, setlasttName] = useState();
+    const [country, setCountry] = useState();
+    const [street, setStreet] = useState();
+    const [zip, setZip] = useState();
+    const [state, setState] = useState();
 
 
     return (
@@ -16,49 +17,61 @@ function SignUp() {
                     <label>
                         First Name:
                     </label>
-                    <input type="text" name="firstName" />
+                    <input
+                        onChange={event => setFirstName(event.target.value)}
+                        type="text" name="firstName" />
                 </div>
-
                 <div>
                     <label>
                         Last Name:
                     </label>
-                    <input type="text" name="lastName" />
+                    <input
+                        onChange={event => setlasttName(event.target.value)}
+                        type="text" name="lastName" />
                 </div>
                 <div>
                     <label>
                         Country:
                     </label>
-                    <input type="text" name="country" />
+                    <input
+                        onChange={event => setCountry(event.target.value)}
+                        type="text" name="country" />
                 </div>
                 <div>
                     <label>
                         Steet:
                     </label>
-                    <input type="text" name="street" />
+                    <input
+                        onChange={event => setStreet(event.target.value)}
+                        type="text" name="street" />
                 </div>
                 <div>
                     <label>
                         Zip:
                     </label>
-                    <input type="text" name="zip" />
+                    <input
+                        onChange={event => setZip(event.target.value)}
+                        type="text" name="zip" />
                 </div>
                 <div>
                     <label>
                         State:
                     </label>
-                    <input type="text" name="state" />
+                    <input
+                        onChange={event => setState(event.target.value)}
+                        type="text" name="state" />
                 </div>
                 <div>
                     <button className="submitButton" type="submit">Submit</button>
                 </div>
             </form>
-            
+
             <p>{firstName}</p>
             <p>{lastName}</p>
             <p>{country}</p>
             <p>{street}</p>
             <p>{zip}</p>
+            <p>{state}</p>
 
 
         </div>
